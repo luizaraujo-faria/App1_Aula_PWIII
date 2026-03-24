61c646fc87067ee2f3dfb98e1ea27186dbe8fe14
luiz <luiz308araujofarias@gmail.com>
2026-03-24 09:24:39 -0300

    Tela de Atualização de Usuarios

---
+++ PrimeiroApp/Controllers/UsuarioController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PrimeiroApp.Models;
+using PrimeiroApp.Repositories;
+using PrimeiroApp.Repositories.Contracts;
+
+namespace PrimeiroApp.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        private IUsuarioRepository _repository;
+        public UsuarioController(IUsuarioRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_repository.ObterUsuarios());
+        }
+
+        [HttpGet]
+        public IActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                _repository.Cadastrar(usuario);
+            }
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult AtualizarUsuario(int id)
+        {
+            return View(_repository.ObterUsuario(id));
+        }
+        [HttpPost]
+        public IActionResult AtualizarUsuario(Usuario usuario)
+        {
+            _repository.Atualizar(usuario);
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ PrimeiroApp/Repositories/UsuarioRepository.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using PrimeiroApp.Models;
+using PrimeiroApp.Repositories.Contracts;
+using System.Data;
+
+namespace PrimeiroApp.Repositories
+{
+    public class UsuarioRepository : IUsuarioRepository
+    {
+        private readonly string _conexaoMySql;
+        public UsuarioRepository(IConfiguration conf)
+        {
+            _conexaoMySql = conf.GetConnectionString("ConexaoMySql");
+        }
+
+        public void Atualizar(Usuario usuario)
+        {
+            using (var conexao = new MySqlConnection(_conexaoMySql))
+            {
+                conexao.Open();
+                MySqlCommand cmd = new MySqlCommand("Update usuario set nomeUsu=@nomeUsu, Cargo=@Cargo," +
+                                                    "DataNasc=@DataNasc Where IdUsu=@IdUsu", conexao);
+
+                cmd.Parameters.Add("@nomeUsu", MySqlDbType.VarChar).Value = usuario.nomeUsu;
+                cmd.Parameters.Add("@Cargo", MySqlDbType.VarChar).Value = usuario.Cargo;
+                cmd.Parameters.Add("@DataNasc", MySqlDbType.Date).Value = usuario.DataNasc;
+
+                cmd.ExecuteNonQuery();
+                conexao.Close();
+            }
+        }
+
+        public void Cadastrar(Usuario usuario)
+        {
+            using( var conexao = new MySqlConnection(_conexaoMySql))
+            {
+                conexao.Open();
+
+                MySqlCommand cmd = new MySqlCommand("insert into tbUsuario(nomeUsu, Cargo, DataNasc)" +
+                                                " values (@nomeUsu, @Cargo, @DataNasc )", conexao);
+
+                cmd.Parameters.Add("@nomeUsu", MySqlDbType.VarChar).Value = usuario.nomeUsu;
+                cmd.Parameters.Add("@Cargo", MySqlDbType.VarChar).Value = usuario.Cargo;
+                cmd.Parameters.Add("@DataNasc", MySqlDbType.Date).Value = usuario.DataNasc;
+
+                cmd.ExecuteNonQuery();
+                conexao.Close();
+            }
+        }
+
+        public IEnumerable<Usuario> ObterUsuarios()
+        {
+            List<Usuario> UsuarioList = new List<Usuario>();
+            using(var conexao = new MySqlConnection(_conexaoMySql))
+            {
+                conexao.Open();
+
+                MySqlCommand cmd = new MySqlCommand("select * from tbUsuario", conexao);
+
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                conexao.Clone();
+
+                foreach (DataRow dr in dt.Rows)
+                {
+                    UsuarioList.Add(
+                        new Usuario
+                        {
+                            idUsu = Convert.ToInt32(dr["IdUsu"]),
+                            nomeUsu = (string)dr["nomeUsu"],
+                            Cargo = (string)dr["Cargo"],
+                            DataNasc = DateOnly.FromDateTime(Convert.ToDateTime(dr["DataNasc"]))
+                        }
+                    );
+                }
+
+                return UsuarioList;
+            }
+        }
+
+        public void Excluir(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Usuario ObterUsuario(int id)
+        {
+            using( var conexao = new MySqlConnection(_conexaoMySql))
+            {
+                conexao.Open();
+                MySqlCommand cmd = new MySqlCommand("Select * from tbUsuario where idUsu=@idUsu", conexao);
+
+                cmd.Parameters.AddWithValue("@idUsu", id);
+
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                MySqlDataReader dr;
+
+                Usuario usuario = new Usuario();
+                dr = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+                while (dr.Read())
+                {
+                    usuario.idUsu = Convert.ToInt32(dr["idUsu"]);
+                    usuario.nomeUsu = (string)(dr["nomeUsu"]);
+                    usuario.Cargo = (string)(dr["Cargo"]);
+                    usuario.DataNasc = DateOnly.FromDateTime(Convert.ToDateTime(dr["DataNasc"]));
+                }
+
+                return usuario;
+            }
+        }
+    }
+}
